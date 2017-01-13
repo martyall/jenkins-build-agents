cfeba1b846ebd0e47c85e95e1194711937ca4da8
Kishore Bhatia <bhatia.kishore@gmail.com>
2017-01-13 12:18:56 -0500

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+exec 3>&1 1>/dev/null
+USER="jenkins"
+#if [[ $USER == "root" ]]
+#then sudo=""
+#else sudo=sudo
+#fi
+
+echo >&3 "Installing Haskell Stack..."
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 575159689BEFB442
+apt-get update
+apt-get -y install lsb-core
+
+echo "deb http://download.fpcomplete.com/ubuntu $(lsb_release -s -c) main" | tee /etc/apt/sources.list.d/fpco.list
+apt-get update
+apt-get -y --allow-unauthenticated install stack git
